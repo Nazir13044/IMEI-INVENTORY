58a55e75f385c1533aee554c87d4d17a3e76fd86
MD. Nazirul Ahsan <42348515+Nazir13044@users.noreply.github.com>
2019-03-03 19:49:14 +0600

    first commit

---
+++ ImeiAppend/ImeiAppendController.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WcfService2.Services;
+using WCMS_COMMON;
+using WCMS_ENTITY;
+using WCMS_MAIN.HelperClass;
+
+namespace WCMS_MAIN.Controllers
+{
+    public class ImeiAppendController : Controller
+    {
+
+       
+        private readonly ImeiAppendService _imeiAppendService = new ImeiAppendService();
+     
+     
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult InventoryOut()
+        {
+            return View();
+        }
+       
+        public JsonResult GetModelName()
+        {
+            List<tblModel> list = new List<tblModel>();
+            try
+            {
+                list = _imeiAppendService.GetModelName().ToList();
+            }
+            catch (Exception ex)
+            {
+
+            }
+            var objstate = list.Select(x => new { value = x.Model, Id = x.Id }).ToList();
+            return Json(objstate.ToArray(), JsonRequestBehavior.AllowGet);
+        }
+        public JsonResult GetColorName()
+        {
+            List<tblColors> list = new List<tblColors>();
+            try
+            {
+                list = _imeiAppendService.GetColorName().ToList();
+            }
+            catch (Exception ex)
+            {
+
+            }
+            var objstate = list.Select(x => new { value = x.Name, Id = x.Id }).ToList();
+            return Json(objstate.ToArray(), JsonRequestBehavior.AllowGet);
+        }
+        public JsonResult InsertImeiInfo(List<tblIMEI> imeiNumber)
+        {
+            var result = new Result();
+            try
+            {
+
+                
+                foreach (var im in imeiNumber)
+                {
+                    //var newlskkkit = new tblNewBarCodeInventory();
+
+                    var existImei =
+                       _imeiAppendService.ImeiExists(new tblIMEIRecord(){IMEI1 =im.IMEI1 }).FirstOrDefault();
+
+                    if (existImei == null)
+                    {
+                       
+                    result = _imeiAppendService.InsertImeiInfo(im);
+                    }
+                    else
+                    {
+                        im.IMEI1 = existImei.IMEI1;
+                        im.IMEI2 = existImei.IMEI2;
+                        im.Model = existImei.Model;
+                        im.Color = existImei.Color;
+                        im.Sn = existImei.Sn;
+                        im.WO = existImei.WO;
+                        result = _imeiAppendService.InsertImeiInfo(im);
+
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+
+            }
+            return Json(result);
+
+
+        }
+
+
+        public JsonResult InsertImeiInventory(List<tblIMEI> imeiNumber)
+        {
+            var result = new Result();
+            try
+            {
+                foreach (var im in imeiNumber)
+                {
+                    var existImei =
+                       _imeiAppendService.ImeiCheck(new tblIMEI() { IMEI1 = im.IMEI1 });
+
+                    if (existImei == null)
+                    {
+
+                        result = _imeiAppendService.InsertImeiInfo(im);
+                    }
+                    else
+                    {
+                        result.IsSuccess = false;
+                        result.Message = "Current IMEI Already Exists";
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+
+            }
+            return Json(result);
+
+
+        }
+	}
+}
---
+++ ImeiAppend/WCMS_DAL_ImeiAppend.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.Core.Objects;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WCMS_COMMON;
+using WCMS_DAL.Interfaces;
+using WCMS_ENTITY;
+
+namespace WCMS_DAL.Inserting
+{
+    public class WCMS_DAL_ImeiAppend : IImeiAppend
+    {
+        private readonly WCMSEntities _entity = new WCMSEntities();
+        public List<tblModel> GetModelName()
+        {
+            List<tblModel> list;
+            try
+            {
+                list = _entity.tblModel.ToList();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+
+            return list;
+        }
+        public List<tblColors> GetColorName()
+        {
+            List<tblColors> list;
+            try
+            {
+                list = _entity.tblColors.ToList();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+
+            return list;
+        }
+
+        public bool InsertImeiInfo(tblIMEI im)
+        {
+            try
+            {
+                _entity.tblIMEI.Add(im);
+                _entity.SaveChanges();
+
+            }
+            catch (Exception exception)
+            {
+
+                throw exception;
+
+            }
+
+            return true;
+        }
+
+        public bool InsertNewImeiInfo(tblIMEI imei)
+        {
+            try
+            {
+                tblNewBarCodeInven va=new tblNewBarCodeInven();
+                va.IMEI1 = imei.IMEI1;
+                va.IMEI2 = imei.IMEI2;
+                va.Model = imei.Model;
+                va.Color = imei.Color;
+                _entity.tblNewBarCodeInven.Add(va);
+                _entity.SaveChanges();
+
+            }
+            catch (Exception exception)
+            {
+
+                throw exception;
+
+            }
+
+            return true;
+        }
+        public List<tblIMEIRecord> ImeiExists(tblIMEIRecord imei)
+        {
+            List<tblIMEIRecord> list;
+            try
+            {
+                list = _entity.tblIMEIRecord.Where(x=>x.IMEI1==imei.IMEI1 || x.IMEI2==imei.IMEI1).ToList();
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+
+            return list;
+        }
+
+        public tblIMEI ImeiCheck(tblIMEI imei)
+        {
+            tblIMEI im;
+            try
+            {
+                im = _entity.tblIMEI.FirstOrDefault(x => x.IMEI1 == imei.IMEI1 || x.IMEI2 == imei.IMEI1);
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+
+            return im;
+        }
+
+      
+
+       
+    }
+}
